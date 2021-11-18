7c20b0868dcf798fcc5f1732fdbc1c09b07c93f1
mattia-gallinaro <72882041+mattia-gallinaro@users.noreply.github.com>
2021-11-18 10:19:23 +0100

    Primo es

---
+++ ClassiDerivate/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace ClassiDerivate
+{
+    // Classe base.
+    class Persona
+    {
+        public int Badge { get; set; }
+        public string Cognome { get; set; }
+        public string Nome { get; set; }
+
+        public void StampaInformazioni()
+        {
+            Console.WriteLine("Badge: {0} Cognome: {1} Nome: {2} ", Badge.ToString(), Cognome, Nome);
+        }
+    }
+    // Prima classe derivata.
+    // Dipendente deriva da Persona
+    // eredita gli attributi ed il metodo da Persona
+    class Dipendente : Persona
+    {
+        public int Matricola { get; set; }
+        public int Retribuzione { get; set; }
+        new public void StampaInformazioni()
+        {
+            //Console.WriteLine("Badge: {0} Cognome: {1} Nome: {2} Matricola: {3} Retribuzione: {4}", Badge.ToString(), Cognome, Nome, Matricola, Retribuzione);
+            base.StampaInformazioni();
+            Console.WriteLine("Matricola: {0} Retribuzione: {1}", Matricola, Retribuzione);
+        }
+    }
+
+    // Seconda classe derivata.
+    // Collaboratore deriva da Persona
+    class Collaboratore : Persona
+    {
+        public string Azienda { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p = new Persona()
+            {
+                Badge = 1,
+                Cognome = "Da Vinci",
+                Nome = "Leonardo"
+            };
+            Dipendente d = new Dipendente()
+            {
+                Badge = 2,
+                Cognome = "Buonarroti",
+                Nome = "Michelangelo",
+                Matricola = 1,
+                Retribuzione = 100
+            };
+            Collaboratore c = new Collaboratore()
+            {
+                Badge = 3,
+                Cognome = "Vecellio",
+                Nome = "Tiziano",
+                Azienda = "Abc"
+            };
+            p.StampaInformazioni();
+            d.StampaInformazioni();
+            c.StampaInformazioni();
+        }
+    }
+}
---
+++ ContaSoldi/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace ContaSoldi
+{
+    class Banca
+    {
+        public double Conto { get; set; }
+        public void Versa(double soldi)
+        {
+            Conto += soldi;
+        }
+        public void Preleva(double soldi)
+        {
+            Conto -= soldi;
+        }
+
+        public void SetConto(double soldi)
+        {
+            Conto = soldi;
+        }
+        public double StampaConto()
+        {
+            return Conto;
+        }
+    }
+    class ContoCorrente : Banca
+    {
+        
+        public bool Preleva(string risposta)
+        {
+            
+            try
+            {
+                double soldi = double.Parse(risposta);
+                if (soldi < 0 || soldi > 3000 || (Conto - soldi) < 0)
+                {
+                    Console.WriteLine("Prelievo non riuscito");
+                    return false;
+                }
+                else
+                {
+                    base.Preleva(soldi);
+                    return true;
+
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public bool Versa(string risposta)
+        {
+            try
+            {
+                double soldi = double.Parse(risposta);
+                if (soldi < 0 || soldi > 3000)
+                {
+                    Console.WriteLine("Versamento non riuscito");
+                    return false;
+                }
+                else
+                {
+                    Versa(soldi);
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+              
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double scelta;
+            ContoCorrente c = new ContoCorrente();
+            Console.WriteLine("Inserisci il tuo conto corrente");
+            scelta = double.Parse(Console.ReadLine());
+            c.SetConto(scelta);
+            string risposta;
+            Console.WriteLine("Inserisci la quantità di soldi da versare");
+            risposta = Console.ReadLine();
+            while (!c.Versa(risposta))
+            {
+                Console.WriteLine("Inserisci la quantità di soldi da versare");
+                risposta = Console.ReadLine();
+            }
+            Console.WriteLine("Inserisci la quantità di soldi da prelevare");
+            risposta = Console.ReadLine();
+            while (!c.Preleva(risposta))
+            {
+                Console.WriteLine("Inserisci la quantità di soldi da prelevare");
+                risposta = Console.ReadLine();
+            }
+            Console.WriteLine("Il tuo saldo e': {0}", Convert.ToString(c.StampaConto()));
+        }
+    }
+}
